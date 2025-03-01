45e4284015c6f623b31ac95c5f51312f095d3613
akashshukla748972 <shuklaakash748972@gmail.com>
2025-03-01 20:45:28 +0530

    setup built-in middleware

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,9 @@
 import express from "express";
+import cookieParser from "cookie-parser";
 
 export const app = express();
+
+// setup built-in middleware
+app.use(cookieParser());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
